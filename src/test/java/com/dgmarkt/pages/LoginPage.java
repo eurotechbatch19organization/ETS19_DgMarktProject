43662b40736cc08e3a0a01e5b593bfe1e3750734
syuleyman <syuleymandouble@gmail.com>
2025-11-26 10:30:33 +0100

    Login methods and login flow are ready

---
+++ src/test/java/com/dgmarkt/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package com.dgmarkt.pages;
+
+
+import com.dgmarkt.utilities.BrowserUtils;
+import com.dgmarkt.utilities.ConfigurationReader;
+import com.dgmarkt.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(name = "email")
+    private WebElement emailBox;
+
+    @FindBy(name = "password")
+    private WebElement passwordBox;
+
+    @FindBy(css = "input[name='login']")
+    private WebElement loginBtn;
+
+    @FindBy(css = "a.a-close-newsletter")
+    private WebElement closeNewsletterButton;
+
+    @FindBy(css = "a[title='My Account']")
+    public WebElement myAccountLink;
+
+    public void login() {
+        emailBox.sendKeys(ConfigurationReader.get("firstEmail"));
+        passwordBox.sendKeys(ConfigurationReader.get("firstPassword"));
+        loginBtn.click();
+        closeNewsletterPopupIfExists();
+    }
+
+    private void closeNewsletterPopupIfExists() {
+        try {
+            BrowserUtils.waitForClickablility(closeNewsletterButton, 10).click();
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void setUpUI(){
+       Driver.get().manage().window().maximize();
+       Driver.get().get(ConfigurationReader.get("url"));
+       login();
+    }
+}
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -2,5 +2,6 @@ Feature: Login Scenarios
 
   Scenario: First Login
     Given User is on the main login page
-    When User enters valid credentials
-    Then User verfies that main page is visible
+    When The user enters valid email and password and clicks the Login button
+    And Closes the newsletter popup if it appears
+    Then The user should see the main page
