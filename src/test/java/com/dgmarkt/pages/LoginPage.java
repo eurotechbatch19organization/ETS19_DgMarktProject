ccda899d6fb0d41d4767aad9fe6d5a826c0983ca
SametEmsen <146114142+SametEmsen@users.noreply.github.com>
2025-12-06 00:19:03 +0300

    Add ExpectedConditions import to LoginPage

--- src/test/java/com/dgmarkt/pages/LoginPage.java
+++ src/test/java/com/dgmarkt/pages/LoginPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 public class LoginPage extends BasePage {
